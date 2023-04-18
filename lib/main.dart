5b505bb9a9baaf3dfc348467379f2ae1041e206e
Pr-att <pp56355@gmail.com>
2023-04-19 01:06:14 +0530

    Minor Update #1

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,27 @@ import 'package:amazon_clone/features/auth/screens/auth_screen.dart';
 import 'package:amazon_clone/router.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+// Used for viewing in almost every possible screens.
+// import 'package:device_preview/device_preview.dart';
+// void main() => runApp(
+//       DevicePreview(
+//         builder: (context) => const MyApp(),
+//       ),
+//     );
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      onGenerateRoute: (settings) => generateRoute(settings),
-        title: 'Flutter Demo',
+      // Used for viewing in almost every possible screens.
+      // useInheritedMediaQuery: true,
+      //   locale: DevicePreview.locale(context),
+      //   builder: DevicePreview.appBuilder,
+        onGenerateRoute: (settings) => generateRoute(settings),
+        title: 'Amazon Clone',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
             scaffoldBackgroundColor: Constants.backgroundColor,
